7125592c34675bff5b85f5fe3f1bf2f45f7f2422
Stefan Nygren <Stefan.Nygren@maquet.com>
2012-11-27 16:00:50 +0100

    moved login back into main class.
    Added requirement to BeautifulSoup (pip install BeautifulSoup)

--- main.py
+++ main.py
@@ -9,9 +9,24 @@ from ntlm import HTTPNtlmAuthHandler
 import os
 import sys
 import cairoplot
-import htmldata
-from login import login
 
+from BeautifulSoup import BeautifulSoup
+
+        
+def login(url, username, password):
+    parsed_url = urlparse(url)
+    base_uri = urlunparse((parsed_url[0],parsed_url[1],"","","",""))
+    passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
+    passman.add_password(None, base_uri, username, password)
+    auth_NTLM = HTTPNtlmAuthHandler.HTTPNtlmAuthHandler(passman)
+    auth_basic = urllib2.HTTPBasicAuthHandler(passman)
+    auth_digest = urllib2.HTTPDigestAuthHandler(passman)
+    proxy_handler = urllib2.ProxyHandler({})
+
+    opener = urllib2.build_opener(proxy_handler, auth_NTLM, auth_digest, auth_basic)
+    urllib2.install_opener(opener)
+    response = urllib2.urlopen(url) 
+    return response.read()
 
 
 def main(task="3773"):
@@ -23,9 +38,10 @@ def main(task="3773"):
     url="https://intranet.hiq.se/activity/printactivity.aspx?id="+task
     uname = '%s\%s' % (domain,uname)
     data=login(url,uname,pwd)
-    data=data.split("table")
-    print data[1]
+    soup = BeautifulSoup(data)
+    print soup.prettify()
 
+    
 def testPlot():
         #Stack horizontal
     #Stack vertical
