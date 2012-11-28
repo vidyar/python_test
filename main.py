41268221130975f9ae57d1c63d369fc7b77e005f
Stefan Nygren <Stefan.Nygren@maquet.com>
2012-11-28 10:55:44 +0100

    Added external dependencies pycario carioplot Added configuration file test.conf

--- main.py
+++ main.py
@@ -9,10 +9,52 @@ from ntlm import HTTPNtlmAuthHandler
 import os
 import sys
 import cairoplot
-
+import ConfigParser
 
 from BeautifulSoup import BeautifulSoup
+def readConfig(task=None,domain=None,uname=None,pwd=None,url=None):
 
+    Config = ConfigParser.ConfigParser()
+    Config.read("test.conf")
+    print Config.sections()
+    dict =ConfigSectionMap(Config,ConfigSectionMap(Config,'Site')['usesite'])
+    print dict
+    for key in dict.keys():
+        if key == 'task':
+            task=dict[key]
+        if key == 'domain':
+            domain=dict[key]
+        if key == 'username':
+            uname=dict[key]
+        if key == 'password':
+            pwd=dict[key]
+        if key == 'url':
+            url=dict[key]            
+    return readLoginData(task,domain,uname,pwd,url)
+def ConfigSectionMap(Config,section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+def readLoginData(task=None,domain=None,uname=None,pwd=None,url=None):
+    if task == None:
+        task    = raw_input("Enter Task id  :")
+    if domain==None:
+        domain  = raw_input("Enter Domain   :")
+    if uname == None:
+        uname   = raw_input("Enter Username :")
+    if pwd == None:
+        pwd     = getpass.getpass('Enter password :')
+    if url == None:
+        url     = raw_input('Enter URL      :')
+    return task,domain,uname,pwd,url
         
 def login(url, username, password):
     parsed_url = urlparse(url)
@@ -29,13 +71,12 @@ def login(url, username, password):
     response = urllib2.urlopen(url) 
     return response.read()
 
-
 def main(task="3773"):
-    task    = getpass.getpass('Enter Task id  :')
-    domain  = getpass.getpass('Enter Domain   :')
-    uname   = getpass.getpass('Enter Username :')
-    pwd     = getpass.getpass('Enter password :')
-    url     = getpass.getpass('Enter URL :')
+
+    if os.path.isfile('test.conf'):
+        (task,domain,uname,pwd,url)=readConfig()
+    else:
+        (task,domain,uname,pwd,url)=readLoginData()
     url="https://"+url+"/activity/printactivity.aspx?id="+task
     uname = '%s\%s' % (domain,uname)
     data=login(url,uname,pwd)
@@ -47,33 +88,50 @@ def main(task="3773"):
     del sres[0]
     del sres[0]
     del sres[-1]
+    res = []
     for row in sres:
-        pos=0
+        aY=0
+        aN=0	
+        aNA=0
+        found=False
         name=row[1].replace('&nbsp','')
         answer=row[2]+row[3]
         answer=answer.replace('&nbsp;','')
         if answer=='Y':
-            print name,"is comming"
+            aY=1
         elif answer=='N':
-            print name,"is not comming"
+            aN=1
         else:
-            print name,"havent replied",answer
+            aNA=1
                 
+        for group in res:
+          if name == group[0]:
+            found=True
+        if found == False:
+          res.append([name,0,0,0])
+        for group in res:
+            if name == group[0]:
+              group[1] += aY
+              group[2] += aN
+              group[3] += aNA
 
-    
 
+    plotData = dict()
+    plotName = []
+    for rec in res:
+      plotData[rec[0]] = [rec[1],rec[2],rec[3]]
+    print res
+    testPlot(plotData,plotData.keys(),'commingGoing')
+    print 
+def testPlot(data,plotName,plotFileName):
 
-
-def testPlot():
-        #Stack horizontal
-    #Stack vertical
-    data = { 'teste00' : [27,13], 'teste01' : [10,20], 'teste02' : [18,12], 'teste03' : [5,25], 'teste04' : [1,29], 'teste05' : [22,8] }
+    plotFileName += '.svg'
     colorGreen = (0,1,0)
     colorRed = (1,0,0)
     colorYellow=(1,1,0.0)
     colors = [ colorGreen, colorRed,  colorYellow]
-    cairoplot.vertical_bar_plot ( 'vbar_0_dictionary.svg', data, 400, 300, border = 20, display_values = True, grid = True, rounded_corners = True, stack=True,colors = colors )
-    
+    cairoplot.vertical_bar_plot ( plotFileName, data, 1920, 1080, border = 20, display_values = True, grid = True, rounded_corners = True, stack=True,colors = colors, x_labels=plotName )
+    print "wrote",plotName
     
     
         
